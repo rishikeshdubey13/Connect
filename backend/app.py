95a6a3e15c00ead9353bca6df5a86e9997c8b525
Rishikesh Dubey <117086580+rishikeshdubey13@users.noreply.github.com>
2025-04-11 02:28:02 +0100

    added the index template

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
@@ -8,8 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
-def home():
-    return "Hello FLask"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 5001, debug=True)
